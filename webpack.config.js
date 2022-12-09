e126bac38495e6f3315cd438465c8d17443db1c6
hblee <vkak006@datasolution.kr>
2022-12-09 16:22:58 +0900

    Feat: 프로젝트 초기화

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  devtool: "eval", //prod : hidden-source-map
+  devServer: {
+    port: 3001,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  entry: {
+    app: "./src/index.tsx",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: "./src/index.html"}),
+    new CleanWebpackPlugin(),
+  ],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.js",
+  },
+};
